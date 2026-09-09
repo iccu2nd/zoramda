b251587ce64f5fd951a44060ebcc5edd16ac9a08
iccu2nd <rezoraxd@gmail.com>
2026-09-09 20:30:47 +0700

    Update via rezora bot (2026-09-09T13:30:46.940Z)

--- src/db/models/SessionConfig.js
+++ src/db/models/SessionConfig.js
@@ -31,7 +31,6 @@ const sessionConfigSchema = new mongoose.Schema(
     antiSpam: { type: Boolean, default: true },
     antiSpamCooldownMs: { type: Number, default: 2000 },
     readMessages: { type: Boolean, default: false },
-    streakEnabled: { type: Boolean, default: true },
     sendTyping: { type: Boolean, default: false },
     sendRecording: { type: Boolean, default: false },
 
